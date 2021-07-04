b75ecd1a36e41223108e3775cb98a060808a6b45
Ayush_S <60178558+Ayushsusheel@users.noreply.github.com>
2021-07-05 02:41:36 +0530

    my_dear_elements_move_towards_left..hehe...

--- Arrays/move_-ve_elements_to_left.java
+++ Arrays/move_-ve_elements_to_left.java
@@ -1,8 +1,55 @@
 //we will be using partition approach for this .....
 
+
+import java.util.*;
+
 public class arrays
 {
-public static void main(String args)
+public static int[] check(int a[],int size)
+{
+int j=0;
+  for(int i=0;i<a.length;i++)
+  {
+  if(a[i]<0)
+  {
+  if(i!=j)
+  {
+  
+  int temp=a[i];
+    a[i]=a[j];
+    a[j]=tmp;
+  
+  
+  }
+  j++;
+  }
+  }
+
+return a;
+
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  int size=sc.nextInt();
+  int a[]=new int[size];
+  for(int i=0;i<a.length;i++)
+  {
+  a[i]=sc.nextInt();
+  }
+  a=check(a,size);
+  System.out.println(Arrays.toString(a));
+  
+}
 
 
 }
