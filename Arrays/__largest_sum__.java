bbafa8c3ca607b5ec71861c31fe97b03e9136c4a
Ayush_S <60178558+Ayushsusheel@users.noreply.github.com>
2021-07-09 04:06:29 +0530

    Update __largest_sum__.java

--- Arrays/__largest_sum__.java
+++ Arrays/__largest_sum__.java
@@ -1,21 +1,21 @@
 import java.util.*;
-
+import java.io.*;
 public  class ARRAYS
 {
 	
 	public static int solution(int a[],int size)
 	{
-		int max_sum=0,curr_sum=0;
+		int max_sum=Integer.MIN_VALUE,curr_sum=0;
 		for(int i=0;i<a.length;i++)
 		{
 			curr_sum=curr_sum+a[i];
-			if(curr_sum>max_sum)
+			if(curr_sum<a[i])
+			{
+				curr_sum=a[i];
+			}
+			 if(max_sum<curr_sum)
 			{
 				max_sum=curr_sum;
-			}
-			else if(curr_sum<0)
-			{
-				curr_sum=0;
 			}
 			
 		}
@@ -24,7 +24,7 @@ public  class ARRAYS
 	
 	
 	
-	public static void main(String...axyzbcde)
+	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
 		int size=sc.nextInt();
