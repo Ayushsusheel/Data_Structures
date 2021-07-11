9e71baac09ae500aff183d8035c7ddf88ad9d55d
Ayush_S <60178558+Ayushsusheel@users.noreply.github.com>
2021-07-12 02:27:52 +0530

    __right_rotate__

--- Arrays/right_rotation.java
+++ Arrays/right_rotation.java
@@ -43,14 +43,17 @@ public class Main
     {
      Scanner sc=new Scanner(System.in);
      
-     
+      System.out.println("enter size of array--->");
      int size=sc.nextInt();
+	System.out.println("how many rotations?????");	
+		int data=sc.nextInt();
      int a[]=new int[size];
+		System.out.println("now enter elements of array--->");
      for(int i=0;i<size;i++)
      {
          a[i]=sc.nextInt();
      }
-     int data=sc.nextInt();
+     
      rotate(a,data);
      for(int i=0;i<a.length;i++)
      {
