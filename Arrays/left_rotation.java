9b3aa919c750ebfde5cf9fef0e7525f396089cce
Ayush_S <60178558+Ayushsusheel@users.noreply.github.com>
2021-07-17 22:10:27 +0530

    Update left_rotation.java

--- Arrays/left_rotation.java
+++ Arrays/left_rotation.java
@@ -66,3 +66,81 @@ public class Main
      
     }
 }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------Any no of inputs........not only 1 i/p...............
+
+// public class ARRAYS
+// {
+// public static void reverse(int a[],int starting,int last)
+
+// {
+ 
+// int i=starting;
+// int j=last;
+// while(i<=j)
+// {
+//     int temp=a[i];
+//     a[i]=a[j];
+//     a[j]=temp;
+//     i++;j--;
+// }
+
+
+    
+// }
+// public static void rotate(int a[],int k)
+// {
+//     k=k%a.length;
+//     if(k<=0)
+//     {
+//         k=k+a.length;
+//     }
+//     reverse(a,0,k-1);
+//     reverse(a,k,a.length-1);
+//     reverse(a,0,a.length-1);
+    
+    
+    
+// }
+
+
+
+
+
+
+
+// 	public static void main(String arhh[])
+// 	{
+// 		Scanner sc=new Scanner(System.in);
+// 		System.out.println("enter test case");
+// 		int testcase=sc.nextInt();
+		
+// 		for(int i=0;i<testcase;i++)
+// 		{
+			
+// 			System.out.println("enter size");
+// 			int size=sc.nextInt();
+// 			System.out.println("enter rotation");
+// 			int ro=sc.nextInt();
+// 			int a[]=new int [size];
+// System.out.println("enter ele");
+// for(int x=0;x<a.length;x++)
+// {
+// 	a[x]=sc.nextInt();
+// }
+// rotate(a,ro);		
+// for(int val:a )
+// {
+// 	System.out.print(val+" ");
+// }
+
+// }
+
+		
+		
+		
+// }}
+
+
+
+
